384214ed37b69dc74ef36cbcd08740f127826371
soychen2025 <soychen2018@gmail.com>
2025-11-27 14:33:36 +0800

    新增檔案傳輸

--- client.py
+++ client.py
@@ -12,6 +12,23 @@ def receive():
             msg = client.recv(1024).decode('utf-8')
             if msg == 'NICK':
                 client.send(nickname.encode('utf-8'))
+            elif msg=='FILE':
+                file_name=client.recv(1024).decode('utf-8')
+                file_size=client.recv(1024).decode('utf-8')
+                sender=client.recv(1024).decode('utf-8')
+                print(f"name={file_name},size={file_size},sender={sender}")
+                try:
+                    with open(f'{file_name}', 'wb') as f:
+                        received_size = 0
+                        while received_size < int(file_size):
+                            data = client.recv(1024)
+                            f.write(data)
+                            received_size += len(data)
+                        f.close()
+                        print(f"{sender} send:{file_name}")
+                except FileNotFoundError:
+                    print('file does not exist!')
+                    break
             else:
                 print(msg)
         except:
@@ -22,13 +39,30 @@ def receive():
 def write():
     while True:
         message = f"{nickname}: {input('')}"
-
         if message == f"{nickname}: EXIT":
             client.send(f"{nickname} has left the chat.\n".encode('utf-8'))
             client.close()
             break
-
-        client.send(message.encode('utf-8'))
+        index_of_send=message.find('send:')
+        file_name=""
+        if index_of_send>=0:
+            for i in range(index_of_send,len(message)):
+                if message[i]==':':
+                    file_name=message[i+1:]
+                    break
+            client.send('FILE'.encode('utf-8'))
+            client.send(file_name.encode('utf-8'))
+            try:
+                with open(file_name, 'rb') as f:
+                    data = f.read()
+                    client.send(str(len(data)).encode('utf-8')) # 傳送檔案大小
+                    client.send(data)
+                    print(f"send:{file_name}")
+                    f.close()
+            except FileNotFoundError:
+                print("file does not exist!")
+        else:
+            client.send(message.encode('utf-8'))
 
 recv_thread = threading.Thread(target=receive)
 recv_thread.start()
--- server.py
+++ server.py
@@ -13,14 +13,54 @@ clients = []
 nicknames = []
 
 def broadcast(message):
+        
     for client in clients:
         client.send(message)
+    
 
 def handle(client):
     while True:
         try:
             msg = client.recv(1024)
-            broadcast(msg)
+            if msg.decode('utf-8')=='FILE':
+                print("ready to receive file!!!")
+                broadcast('FILE'.encode('utf-8'))
+                file_name=client.recv(1024)
+                file_size=client.recv(1024)
+                sender=nicknames[clients.index(client)]
+                print(f"name={file_name},size={file_size},sender={sender}")
+                broadcast(file_name)
+                print("broadcast file name completed!!")
+                broadcast(file_size)
+                print("brocast file size completed!!")
+                broadcast(sender.encode('utf-8'))
+                print("fininsh broadcast!!!!")
+                file_size_int=int(file_size.decode('utf-8'))
+                file_name=file_name.decode('utf-8')
+                print(f"file size is{file_size_int}Bytes")
+                try:
+                    with open(f'{file_name}', 'wb') as f:#寫入
+                        received_size = 0
+                        while received_size < file_size_int:
+                            data = client.recv(1024)
+                            f.write(data)
+                            received_size += len(data)
+                        print(f"{sender} send:{file_name}")
+                        f.close()
+                except FileNotFoundError:
+                    print('file does not exist!')
+                    break
+                try:
+                    with open(f'{file_name}', 'rb') as t:
+                        print("reading!")
+                        data=t.read()
+                        broadcast(data)
+                        t.close()
+                except FileNotFoundError:
+                    print('file does not exist!')
+                    break
+            else:
+                broadcast(msg)   
         except:
             index = clients.index(client)
             clients.remove(client)
